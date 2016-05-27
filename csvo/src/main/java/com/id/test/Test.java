37e456da9700cbeae5ae3f9c399cee038837f722
yufh <670915333@qq.com>
2016-05-27 15:51:23 +0800

    添加字典类注解解析

--- csvo/src/main/java/com/id/test/Test.java
+++ csvo/src/main/java/com/id/test/Test.java
@@ -81,5 +81,41 @@ public class Test {
 		if(m.find()) {
 			System.out.println("================" + m.group());
 		}
+		
+		System.out.println("++++++++++++++++++++++++");
+		System.out.println(getRemainingPath("/news/main/list.jsp", "/news", true));
+	}
+	
+	private static String getRemainingPath(String requestUri, String mapping, boolean ignoreCase) {
+		int index1 = 0;
+		int index2 = 0;
+		for ( ; (index1 < requestUri.length()) && (index2 < mapping.length()); index1++, index2++) {
+			char c1 = requestUri.charAt(index1);
+			char c2 = mapping.charAt(index2);
+			if (c1 == ';') {
+				index1 = requestUri.indexOf('/', index1);
+				if (index1 == -1) {
+					return null;
+				}
+				c1 = requestUri.charAt(index1);
+			}
+			if (c1 == c2) {
+				continue;
+			}
+			if (ignoreCase && (Character.toLowerCase(c1) == Character.toLowerCase(c2))) {
+				continue;
+			}
+			return null;
+		}
+		if (index2 != mapping.length()) {
+			return null;
+		}
+		if (index1 == requestUri.length()) {
+			return "";
+		}
+		else if (requestUri.charAt(index1) == ';') {
+			index1 = requestUri.indexOf('/', index1);
+		}
+		return (index1 != -1) ? requestUri.substring(index1) : "";
 	}
 }
